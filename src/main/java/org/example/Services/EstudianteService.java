b0756d7dd6de39fcc81021d3d584f14a8b18fabe
Sebastian Ortega <sebastiangastonortega@outlook.com>
2024-10-12 00:35:15 -0300

    Corregido error en EstudianteController

--- src/main/java/org/example/Services/EstudianteService.java
+++ src/main/java/org/example/Services/EstudianteService.java
@@ -5,6 +5,7 @@ import org.example.Repositories.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.example.Services.*;
 import org.example.Entities.*;
@@ -17,6 +18,7 @@ public class EstudianteService {
     private EstudianteRepository estudianteRepository;
 
     // a) Dar de alta un estudiante
+    @Transactional
     public Estudiante insertarEstudiante(Estudiante estudiante) {
         return estudianteRepository.save(estudiante);
     }
